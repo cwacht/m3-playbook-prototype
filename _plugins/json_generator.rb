67f4c1382be12c916eead1cdda7939d8b2e0d51a
cwacht <cwachtman@gmail.com>
2021-03-27 22:18:44 -0400

    cleanup

--- _plugins/json_generator.rb
+++ _plugins/json_generator.rb
@@ -6,13 +6,9 @@ module Jekyll
     safe true
 
     def generate(site)
-
       site.collections["pages"].each do |page|
-        # puts page
-        # puts page.data["permalink"]
         render_json(page,site)    
       end
-
     end
 
     def render_json(post, site)
@@ -21,21 +17,9 @@ module Jekyll
       if post.data.has_key? "json" and !post.data["json"]
         return
       end
-
-      # path = post.destination( site.source )
+      
       path = "_site/static-api" + post.url + "/data.json"
-
-      #only act on post/pages index in /index.html
-      # return if /\/index\.html$/.match(path).nil?
-
-      #change file path
-      # path['/index.html'] = '.json'
-
-      #render post using no template(s)
-      # post.render( {}, site.site_payload)
-
-      #prepare output for JSON
-      # post.data["related_posts"] = related_posts(post,site)
+      
       output = [
         "url": post.to_liquid["url"],
         "name": post.to_liquid["name"],
@@ -54,13 +38,7 @@ module Jekyll
         "tollgateInfo": post.to_liquid["tollgateInfo"],
         "exitCriteria": post.to_liquid["exitCriteria"]
       ]
-      # output = post.to_liquid
-      # output["next"] = output["next"].id unless output["next"].nil?
-      # output["previous"] = output["previous"].id unless output["previous"].nil?
 
-      #write
-      #todo, figure out how to overwrite post.destination 
-      #so we can just use post.write
       FileUtils.mkdir_p(File.dirname(path))
       File.open(path, 'w') do |f|
         f.write(output.to_json)
